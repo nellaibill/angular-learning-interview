2c8e37c240390c9130c128d2659dc1fb56b7dced
Saleem <nellaibill@gmail.com>
2023-12-27 09:26:52 +0530

    Added : Reactive Forms-Customvalidator,FormBuilder,FormArray

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
 import { SharableService } from './service/sharable.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from './service/user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CoursesPipesComponent } from './courses-pipes.component';
@@ -13,6 +13,7 @@ import { BasicConcepts1Component } from './basic-concepts1/basic-concepts1.compo
 import { CustomPipe } from './custom-pipe.component';
 import { BasicConcepts2Component } from './basic-concepts2/basic-concepts2.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
@@ -22,13 +23,15 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     BasicConcepts1Component,
     CustomPipe,
     BasicConcepts2Component,
-    ContactFormComponent
+    ContactFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [SharableService,UserService],
   bootstrap: [AppComponent]
